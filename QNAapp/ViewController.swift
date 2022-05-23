e974f374e4a22440aeabe0f8ca06dfd657499049
Yoga <jc051533@gmail.com>
2022-05-23 18:45:41 +0800

    Initial Commit

---
+++ QNAapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QNAapp
+//
+//  Created by 王鈞則 on 2022/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
